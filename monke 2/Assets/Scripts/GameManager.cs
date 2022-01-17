55018c860d7d5cef743be2ab59f3bbde1a6e6c05
ethonk <62923028+ethonk@users.noreply.github.com>
2022-01-17 21:35:12 +1300

    Base Room

---
+++ monke 2/Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("States")]
+    public bool ste_tabletActive = false;
+
+    [Header("Models")]
+    public GameObject mdl_tablet;
+
+    void Update()
+    {   
+        // Update the tablet
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            ste_tabletActive = !ste_tabletActive;
+            mdl_tablet.GetComponent<Animator>().SetBool("tablet-active", ste_tabletActive);
+        }
+    }
+}
---
+++ monke 2/Assets/Scripts/MonkeScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonkeScript : MonoBehaviour
+{
+    [Header("Position Data")]
+    public int positionIndex = 0;   // Do not modify.
+    public Vector3 currentPosition; // Do not modify.
+    public List<Vector3> positions;
+
+    [Header("Movement Data")]
+    public float moveDelay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(MoveMonke());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    // Move the Monke
+    public IEnumerator MoveMonke()
+    {
+        yield return new WaitForSeconds(moveDelay);
+        transform.root.position = positions[positionIndex];
+        currentPosition = positions[positionIndex];
+        positionIndex++;
+
+        if (positionIndex == positions.Count)   // If maximum has reached, reset position.
+        {
+            positionIndex = 0;
+        }
+
+        StartCoroutine(MoveMonke());
+    }
+
+    // Run Jumpscare
+    void Jumpscare()
+    {
+
+    }
+}
